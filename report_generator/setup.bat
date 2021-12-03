2d5182e331818f3964406eb733c185acc5763840
sam <samuel.mao@c9tec.com>
2021-12-03 13:23:13 -0500

    debug setup

--- report_generator/setup.bat
+++ report_generator/setup.bat
@@ -1,3 +1,7 @@
+#! /bin/bash
+set mypath=%cd%
+set gitpath="%ProgramFiles%\Git\bin\"
+
 C:\Python3.7.4\python.exe -m venv ./venv
-%gitpath%bash.exe  -c "\"%mypath%\venv\Scripts\python.exe\"-m pip install --upgrade pip
-%gitpath%bash.exe  -c "\"%mypath%\venv\Scripts\python.exe\" -m pip install -r requirements.txt  >NUL"
+%gitpath%bash.exe  -c "\"%mypath%\venv\Scripts\python.exe\" -m pip install --upgrade pip"
+%gitpath%bash.exe  -c "\"%mypath%\venv\Scripts\python.exe\" -m pip install -r requirements.txt"
