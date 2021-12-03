c17d0352d105923612116f3b042ae0cbe274e200
sam <samuel.mao@c9tec.com>
2021-12-02 19:40:41 -0500

    prod gen report

---
+++ generate_report_2.bat
@@ -0,0 +1,17 @@
+#! /bin/bash
+set mypath=%cd%
+set gitpath="%ProgramFiles%\Git\bin\"
+
+
+rd /s /q "%mypath%\DONE"
+mkdir "%mypath%\DONE"
+
+
+rd /s /q "%mypath%\tmp"
+mkdir "%mypath%\tmp"
+
+%gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" -m pip install -r ./report_generator/requirements.txt  >NUL"
+%gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" ./report_generator/main.py -mode=2"
+
+rd /s /q "%mypath%\tmp"
+Pause
