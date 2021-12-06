39bb2c32535fb43e04097ad35c175949b7d418a8
sam <samuel.mao@c9tec.com>
2021-12-06 13:05:56 -0500

    debug requirements

--- setup.bat
+++ setup.bat
@@ -5,7 +5,4 @@ set gitpath="%ProgramFiles%\Git\bin\"
 C:\Python3.7.4\python.exe -m venv ./report_generator/venv
 %gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" -m pip install --upgrade pip"
 %gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" -m pip install -r ./report_generator/requirements.txt"
-timeout /t 1
-%gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" -m pip install --upgrade pip"
-%gitpath%bash.exe  -c "\"%mypath%\report_generator\venv\Scripts\python.exe\" -m pip install -r ./report_generator/requirements.txt"
 Pause
